1818cbbda6b99e53965bd3604f2cc0c4ac798b17
Ishan Nikeshala <ishannikeshala1999@gmail.com>
2024-07-16 22:18:58 +0530

    design update

--- src/page/Hero.js
+++ src/page/Hero.js
@@ -14,7 +14,7 @@ export const Hero = () => {
         <div className='mx-auto max-w-5xl py-20 sm:py-40 lg:pt-32'>
           <div className='text-center'>
             <div className='flex justify-center items-center'>
-              <img src={img} className='lg:h-80 lg:w-80 h-60 w-60 rounded-full' />
+              <img src={img} className='lg:h-80 lg:w-80 h-72 w-72 rounded-full' />
             </div>
             <div className='lg:mt-8 mt-4'>
               <p className='lg:text-5xl text-2xl font-bold'>Ishan Nikeshala Nawarathna</p>
@@ -37,7 +37,7 @@ export const Hero = () => {
               <FaLinkedin size={30} />
               <FaMedium size={30} />
             </div>
-            <div className='flex justify-center items-center lg:mt-8 mt-4'>
+            <div className='flex justify-center items-center lg:mt-8 mt-4 '>
               <button type="button" className="text-white flex justify-between items-center bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium  rounded-full text-lg px-10 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"><MdDownload />Download CV</button>
             </div>
           </div>

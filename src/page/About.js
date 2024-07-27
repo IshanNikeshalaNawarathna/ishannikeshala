352eab5cc947fa85b2147143cc44330ffc9d5cb9
Ishan Nikeshala <ishannikeshala1999@gmail.com>
2024-07-27 21:50:55 +0530

    design update

--- src/page/About.js
+++ src/page/About.js
@@ -99,13 +99,13 @@ export const About = () => {
               </Timeline>
             </Fragment>
             <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-2 lg:gap-4 mt-10">
-              <div class="lg:row-span-3 row-span-1 justify-center items-center flex shadow-md rounded-2xl">
+              <div class="lg:row-span-3 row-span-1 justify-center items-center flex shadow-md rounded-2xl p-1">
                 <div className="lg:text-[24px] text-[17px] lg:flex items-center font-bold">
                   <div className='lg:pt-8 lg:p-3 pt-5'>
                     <RiCupLine className='lg:text-[84px] text-[50px]' />
                   </div>
                   <div>
-                    <h5 className='lg:text-[24px] text-[16px] font-semibold text-gray-600'>Repositories</h5>
+                    <h5 className='lg:text-[24px] text-[16px] font-semibold text-gray-600'>GitHub Repositories</h5>
                     <div className='flex items-center lg:pb-0 pb-2'>
                       <CountUp isCounting end={50} duration={3.2} />
                       <IoMdAdd size={30} />
@@ -113,7 +113,7 @@ export const About = () => {
                   </div>
                 </div>
               </div>
-              <div class="lg:row-span-3 row-span-1 justify-center items-center flex shadow-md  rounded-2xl">
+              <div class="lg:row-span-3 row-span-1 justify-center items-center flex shadow-md  rounded-2xl p-1">
                 <div className="lg:text-[24px] text-[17px] lg:flex items-center font-bold ">
                   <div className='lg:pt-8 lg:p-3 pt-5'>
                     <GoRocket className='lg:text-[84px] text-[50px]' />
@@ -126,7 +126,7 @@ export const About = () => {
                   </div>
                 </div>
               </div>
-              <div class="lg:row-span-3 row-span-1 justify-center items-center flex shadow-md rounded-2xl">
+              <div class="lg:row-span-3 row-span-1 justify-center items-center flex shadow-md rounded-2xl p-1">
                 <div className="lg:text-[24px] text-[17px] lg:flex items-center font-bold ">
                   <div className='lg:pt-8 lg:p-3 pt-5'>
                     <SlTrophy className='lg:text-[84px] text-[50px]' />
